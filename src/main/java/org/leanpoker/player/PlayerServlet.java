8f6aee3c2368165fc9bea3b5d0330b797f72c683
zsmuller <zsmuller@zsmuller-linux.navngo.local>
2014-05-28 13:54:05 +0200

    ArrayOutOfBounds fix

--- src/main/java/org/leanpoker/player/PlayerServlet.java
+++ src/main/java/org/leanpoker/player/PlayerServlet.java
@@ -17,10 +17,25 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/")
 public class PlayerServlet extends HttpServlet
 {
-    AbstractPlayer playerBasic = new PlayerBasic();
-    AbstractPlayer playerMuzso = new Player();
-    AbstractPlayer playerSmart = new PlayerSmart();
-    AbstractPlayer[] players = new AbstractPlayer[] { playerBasic, playerMuzso, playerSmart };
+    static AbstractPlayer playerBasic = new PlayerBasic();
+    static AbstractPlayer playerMuzso = new Player();
+    static AbstractPlayer playerSmart = new PlayerSmart();
+    static AbstractPlayer[] players = new AbstractPlayer[] { playerBasic, playerMuzso, playerSmart };
+    static AbstractPlayer player = playerSmart;
+
+    static
+    {
+        Random r = new Random();
+        int idx = r.nextInt();
+
+        if (idx < 0)
+        {
+            idx = idx * -1;
+        }
+
+        idx = idx % players.length;
+        player = players[idx];
+    }
 
     @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
@@ -29,11 +44,7 @@ public class PlayerServlet extends HttpServlet
 
     @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        Random r = new Random();
-        int idx = r.nextInt() % players.length;
-        AbstractPlayer player = players[idx];
-
-        System.err.println("AI, player idx: " + idx + ", version: " + player.VERSION);
+        System.err.println("AI, version: " + player.VERSION);
         if (req.getParameter("action").equals("bet_request"))
         {
             String gameState = req.getParameter("game_state");
