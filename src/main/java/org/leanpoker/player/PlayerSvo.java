fcf8cd87c79934782a2dac0873df639ae9a0a5bf
István Svorcz <istvan.svorcz@nng.com>
2014-05-28 16:37:26 +0200

    Specplayer

---
+++ src/main/java/org/leanpoker/player/PlayerSvo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.leanpoker.player;
+
+import org.leanpoker.player.json.GameStatusJson;
+import org.leanpoker.player.json.PlayerJson;
+
+/**
+ *
+ * @author isvorcz
+ */
+public class PlayerSvo extends AbstractPlayer {
+
+    AbstractPlayer other = new PlayerSmart();
+
+    @Override
+    public int betRequest(GameStatusJson status) {
+        int out = 0;
+        for (PlayerJson p : status.players) {
+            if ("out".equals(p.status)) {
+                out++;
+            }
+        }
+        return out < (status.players.size() - 2) ? 0 : other.betRequest(status);
+    }
+
+    @Override
+    public String getVersion() {
+        return "Castle";
+    }
+
+}
