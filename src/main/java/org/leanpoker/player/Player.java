c523ff299bcb715543898eae8e6da0d5a525a784
István Svorcz <istvan.svorcz@nng.com>
2014-05-28 10:34:42 +0200

    párra emelünk

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,14 +6,12 @@ import org.leanpoker.player.json.GameStatusJson;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "Nespresso";
 
-    
-    
     public static int betRequest(GameStatusJson status) {
         List<CardJson> cards = status.players.get(status.in_action).hole_cards;
-        
-        if ( cards.get(0).rank.equals(cards.get(1).rank) && ( status.current_buy_in - status.players.get(status.in_action).bet > status.players.get(status.in_action).stack / 2)) {
+
+        if (cards.get(0).rank.equals(cards.get(1).rank) && (status.current_buy_in - status.players.get(status.in_action).bet > status.players.get(status.in_action).stack / 2)) {
             return status.current_buy_in - status.players.get(status.in_action).bet + status.minimum_raise;
         } else {
             return 0;
