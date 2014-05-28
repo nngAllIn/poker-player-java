039786328e8e132e904600abc920f828f8663b87
KERiii <KERiii@192.168.53.179>
2014-05-28 11:45:32 +0200

    raise added

--- src/main/java/org/leanpoker/player/PlayerSmart.java
+++ src/main/java/org/leanpoker/player/PlayerSmart.java
@@ -21,7 +21,7 @@ public class PlayerSmart extends AbstractPlayer{
         List<CardJson> cards = status.players.get(status.in_action).hole_cards;
         
         if (cards.get(0).rank.equals(cards.get(1).rank) && (status.current_buy_in - status.players.get(status.in_action).bet > status.players.get(status.in_action).stack / 2)) {
-            return status.current_buy_in - status.players.get(status.in_action).bet + status.minimum_raise;
+            return status.current_buy_in - status.players.get(status.in_action).bet + status.minimum_raise * 3;
         } else {
             return 0;
         }
